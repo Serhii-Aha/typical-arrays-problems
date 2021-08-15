fb44670ee037fb0a2bdff8aefcd01a9f0d88d70e
unknown <a1002android@gmail.com>
2021-08-15 23:56:38 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ((array === undefined) || (array == [])) {
+    return 0;
+  }
+  let min = Infinity;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    }
+  }
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if ((array === undefined) || (array == [])) {
+    return 0;
+  }
+  let max = -Infinity;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ((array === undefined) || array == []) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+    }
+  return sum / array.length;
 }
